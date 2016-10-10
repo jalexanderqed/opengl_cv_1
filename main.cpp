74307a7ac1fd0473913eb98461ca2b84a4ff8bad
John Alexander <jalexander.qed@gmail.com>
2016-10-10 10:21:00 -0700

    Finished first triangle and first rectangle tutorial.

--- main.cpp
+++ main.cpp
@@ -15,17 +15,50 @@ using namespace std;
 string shaderStrings[fragment_shaders::FRAGMENT_SHADER_MAX];
 GLuint shaders[fragment_shaders::FRAGMENT_SHADER_MAX];
 
+GLuint basicProgram;
+
+GLuint triangleVAO;
+
 void draw(){
+    glUseProgram(basicProgram);
+    glBindVertexArray(triangleVAO);
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    glBindVertexArray(0);
+}
+
+void initObjects(){
+    glGenVertexArrays(1, &triangleVAO);
+
+    glBindVertexArray(triangleVAO);
     GLfloat vertices[] = {
-            -0.5f, -0.5f, 0.0f,
+            0.5f, 0.5f, 0.0f,
             0.5f, -0.5f, 0.0f,
-            0.0f, 0.5f, 0.0f
+            -0.5f, -0.5f, 0.0f,
+            -0.5f, 0.5f, 0.0f
+    };
+    GLuint indices[] = {
+            0, 1, 3,
+            1, 2, 3
     };
 
     GLuint VBO;
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    GLuint EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
 }
 
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode){
@@ -60,7 +93,7 @@ int compileShaders(){
         if(!success){
             GLchar infoLog[512];
             glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-            cout << "ERROR::SHADER""::VERTEX::COMPILATION_FAILED index:"
+            cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED index:"
                  << i << "\n" << infoLog << endl;
             cout << shaderStrings[i] << endl;
             return -1;
@@ -78,14 +111,30 @@ int compileShaders(){
         if(!success){
             GLchar infoLog[512];
             glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-            cout << "ERROR::SHADER""::FRAGMENT::COMPILATION_FAILED index:"
+            cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED index:"
                  << i << "\n" << infoLog << endl;
             cout << shaderStrings[i] << endl;
             return -1;
         }
         shaders[i] = fragmentShader;
     }
+    return 0;
+}
 
+int createPrograms(){
+    basicProgram = glCreateProgram();
+    glAttachShader(basicProgram, shaders[vertex_shaders::BASIC_VERTEX_SHADER]);
+    glAttachShader(basicProgram, shaders[fragment_shaders::BASIC_FRAGMENT_SHADER]);
+    glLinkProgram(basicProgram);
+
+    GLint success;
+    glGetProgramiv(basicProgram, GL_LINK_STATUS, &success);
+    if(!success){
+        GLchar infoLog[512];
+        glGetShaderInfoLog(basicProgram, 512, NULL, infoLog);
+        cout << "ERROR::PROGRAM::LINKING_FAILED\n" << infoLog << endl;
+        return -1;
+    }
     return 0;
 }
 
@@ -114,6 +163,11 @@ int main() {
     if(compileShaders() != 0){
         return -1;
     }
+
+    if(createPrograms() != 0){
+        return -1;
+    }
+    initObjects();
 
     int width, height;
     glfwGetFramebufferSize(window, &width, &height);
