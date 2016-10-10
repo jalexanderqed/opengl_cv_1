80548ae27739f0c46c1b0f987b82ca0d30f190a8
John Alexander <jalexander.qed@gmail.com>
2016-10-09 23:36:32 -0700

    Restarted project using core profile functionality and CMake.

---
+++ main.cpp
@@ -0,0 +1,138 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+#include <GL/gl.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+#include "util/util.h"
+
+using namespace std;
+
+string shaderStrings[fragment_shaders::FRAGMENT_SHADER_MAX];
+GLuint shaders[fragment_shaders::FRAGMENT_SHADER_MAX];
+
+void draw(){
+    GLfloat vertices[] = {
+            -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f, 0.5f, 0.0f
+    };
+
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode){
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS){
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
+
+void loadShaders(){
+    for(int i = 0; i < vertex_shaders::VERTEX_SHADER_MAX; i++){
+        ostringstream fileName;
+        fileName << "shaders/vert_" << i << ".vert";
+        shaderStrings[i] = (readFile(fileName.str()));
+    }
+
+    for(int i = vertex_shaders::VERTEX_SHADER_MAX; i < fragment_shaders::FRAGMENT_SHADER_MAX; i++){
+        ostringstream fileName;
+        int shaderNum = i - vertex_shaders::VERTEX_SHADER_MAX;
+        fileName << "shaders/frag_" << shaderNum << ".frag";
+        shaderStrings[i] = (readFile(fileName.str()));
+    }
+}
+
+int compileShaders(){
+    for(int i = 0; i < vertex_shaders::VERTEX_SHADER_MAX; i++){
+        GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        const char* cstr = shaderStrings[i].c_str();
+        glShaderSource(vertexShader, 1, &cstr, NULL);
+        glCompileShader(vertexShader);
+        GLint success;
+        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+        if(!success){
+            GLchar infoLog[512];
+            glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+            cout << "ERROR::SHADER""::VERTEX::COMPILATION_FAILED index:"
+                 << i << "\n" << infoLog << endl;
+            cout << shaderStrings[i] << endl;
+            return -1;
+        }
+        shaders[i] = vertexShader;
+    }
+
+    for(int i = vertex_shaders::VERTEX_SHADER_MAX; i < fragment_shaders::FRAGMENT_SHADER_MAX; i++){
+        GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        const char* cstr = shaderStrings[i].c_str();
+        glShaderSource(fragmentShader, 1, &cstr, NULL);
+        glCompileShader(fragmentShader);
+        GLint success;
+        glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+        if(!success){
+            GLchar infoLog[512];
+            glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+            cout << "ERROR::SHADER""::FRAGMENT::COMPILATION_FAILED index:"
+                 << i << "\n" << infoLog << endl;
+            cout << shaderStrings[i] << endl;
+            return -1;
+        }
+        shaders[i] = fragmentShader;
+    }
+
+    return 0;
+}
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "glhw_01", nullptr, nullptr);
+    if(window == nullptr){
+        cout << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = GL_TRUE;
+    if(glewInit() != GLEW_OK){
+        cout << "Failed to initialize GLEW" << endl;
+        return -1;
+    }
+
+    loadShaders();
+    if(compileShaders() != 0){
+        return -1;
+    }
+
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+    glfwSetKeyCallback(window, keyCallback);
+
+    while(!glfwWindowShouldClose(window)){
+        glfwPollEvents();
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        draw();
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
---
+++ util/util.h
@@ -0,0 +1,26 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+string readFile(string name){
+    ifstream ifs(name);
+    string content((istreambuf_iterator<char>(ifs)),
+                   (istreambuf_iterator<char>()));
+    return content;
+}
+
+enum vertex_shaders{
+    BASIC_VERTEX_SHADER = 0,
+    VERTEX_SHADER_MAX
+};
+
+enum fragment_shaders{
+    BASIC_FRAGMENT_SHADER = VERTEX_SHADER_MAX,
+    FRAGMENT_SHADER_MAX
+};
+
+#endif
