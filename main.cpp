bbda93c0b657a97e89c7f69ee2f976afdd4a1bc0
jalexanderqed <jalexander.qed@gmail.com>
2016-10-15 11:40:44 -0700

    Created cube for lighting testing (no normal vectors).

--- main.cpp
+++ main.cpp
@@ -73,7 +73,7 @@ void initObjects() {
 
     glBindVertexArray(triangleVAO);
     GLfloat vertices[] = {
-            0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+            0.5f, 0.5f, 0.3f, 1.0f, 0.0f, 0.0f,
             0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
             -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0,
             -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f
